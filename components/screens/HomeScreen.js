0b71dc8e947ff87ec0f52c22dabb1bef3ab0e387
Shashikant <shashikantpanchal.info@gmail.com>
2023-09-22 13:12:49 +0530

    Refactored HomeScreen

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -67,21 +67,10 @@ const HomeScreen = () => {
                             keyExtractor={(_, index) => String(index)}
                             horizontal
                             inverted
-                            contentContainerStyle={{
-                                flex: 1,
-                                justifyContent: 'center',
-                                padding: SPACING * 2,
-                                marginTop: 50,
-                            }}
+                            contentContainerStyle={{ flex: 1, justifyContent: 'center', padding: SPACING * 2, marginTop: 50, }}
                             scrollEnabled={false}
                             removeClippedSubviews={false}
-                            CellRendererComponent={({
-                                item,
-                                index,
-                                children,
-                                style,
-                                ...props
-                            }) => {
+                            CellRendererComponent={({ item, index, children, style, ...props }) => {
                                 const newStyle = [style, { zIndex: data.length - index }];
                                 return (
                                     <View style={newStyle} index={index} {...props}>
@@ -91,47 +80,17 @@ const HomeScreen = () => {
                             }}
                             renderItem={({ item, index }) => {
                                 const inputRange = [index - 1, index, index + 1];
-                                const translateX = scrollXAnimated.interpolate({
-                                    inputRange,
-                                    outputRange: [50, 0, -100],
-                                });
-                                const scale = scrollXAnimated.interpolate({
-                                    inputRange,
-                                    outputRange: [0.8, 1, 0.5],
-                                });
-                                const opacity = scrollXAnimated.interpolate({
-                                    inputRange,
-                                    outputRange: [1 - 1 / VISIBLE_ITEMS, 1, 0],
-                                });
-
+                                const translateX = scrollXAnimated.interpolate({ inputRange, outputRange: [50, 0, -100], });
+                                const scale = scrollXAnimated.interpolate({ inputRange, outputRange: [0.8, 1, 0.5], });
+                                const opacity = scrollXAnimated.interpolate({ inputRange, outputRange: [1 - 1 / VISIBLE_ITEMS, 1, 0], });
                                 return (
                                     <TouchableOpacity
                                         activeOpacity={1}
                                         onPress={() => {
                                             navigation.navigate('Details', { item });
-                                        }}
-                                    >
-                                        <Animated.View
-                                            style={{
-                                                position: 'absolute',
-                                                left: -ITEM_WIDTH / 2,
-                                                opacity,
-                                                transform: [
-                                                    {
-                                                        translateX,
-                                                    },
-                                                    { scale },
-                                                ],
-                                            }}
-                                        >
-                                            <Image
-                                                source={{ uri: item.poster }}
-                                                style={{
-                                                    width: ITEM_WIDTH,
-                                                    height: ITEM_HEIGHT,
-                                                    borderRadius: 14,
-                                                }}
-                                            />
+                                        }}>
+                                        <Animated.View style={{ position: 'absolute', left: -ITEM_WIDTH / 2, opacity, transform: [{ translateX }, { scale }] }}>
+                                            <Image source={{ uri: item.poster }} style={{ width: ITEM_WIDTH, height: ITEM_HEIGHT, borderRadius: 14 }} />
                                         </Animated.View>
                                     </TouchableOpacity>
                                 );
